3fb2cc78cc6ab26229b71f03e057350a9a710d8f
your name <msantiagogrodriguez@gmail.com>
2025-09-24 21:17:40 -0500

    pasos

--- torres.py
+++ torres.py
@@ -1,10 +1,17 @@
 def hanoi(n, origen, destino, auxiliar):
+    global pasos
+
     if n == 1:
-        print(f"mover el disco 1 de {origen} a {destino}")
+        print(f"paso {pasos}: mover el disco 1 de {origen} a {destino}")
+        pasos += 1
         return
+    
     hanoi(n-1, origen, auxiliar, destino)
-    print(f"mover el disco {n} de {origen} a { destino}")
+    print(f"paso {pasos}mover el disco {n} de {origen} a { destino}")
+    pasos += 1
     hanoi(n-1, auxiliar, destino , origen)
 
+pasos = 1
 num = int(input("ingrese el numero de discos: "))
 hanoi(num, 'A','C','B')
+print(f"el numero de pasos: {pasos - 1}" )
