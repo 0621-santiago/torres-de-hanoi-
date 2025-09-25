8769d44f8555d67c7365b3d7e46a6c93c3d6334d
your name <msantiagogrodriguez@gmail.com>
2025-09-24 21:27:41 -0500

    arreglo de pasos

--- torres.py
+++ torres.py
@@ -7,7 +7,7 @@ def hanoi(n, origen, destino, auxiliar):
         return
     
     hanoi(n-1, origen, auxiliar, destino)
-    print(f"paso {pasos}mover el disco {n} de {origen} a { destino}")
+    print(f"paso {pasos}: mover el disco {n} de {origen} a { destino}")
     pasos += 1
     hanoi(n-1, auxiliar, destino , origen)
 
